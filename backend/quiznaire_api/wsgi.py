05a631a1a9843788f8295ec14aa5e8a156a4e2f5
mohitbhasin <mohit.bhasin16@gmail.com>
2025-09-17 17:26:27 +0530

    updating project name to quiznaire_api

--- backend/quiznaire_api/wsgi.py
+++ backend/quiznaire_api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for quiznaire project.
+WSGI config for quiznaire_api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quiznaire.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quiznaire_api.settings')
 
 application = get_wsgi_application()
